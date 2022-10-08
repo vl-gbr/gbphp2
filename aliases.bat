fc98b72a0a78e923207ab29e92cb3797fdfe5ddf
vl-gbr <vlenxx@yandex.ru>
2022-10-08 20:53:58 +0300

    composers bats

---
+++ aliases.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+
+@REM @echo ALIASES=%ALIASES%
+
+if "%*" == "off" (
+    @REM @echo ALIASES=%ALIASES%
+    @REM @echo Params: %*
+    doskey host= 
+    doskey pd= 
+    doskey wd=cd %CD% 
+    doskey com=composer $*
+    doskey art=
+    @set ALIASES=
+    setlocal enabledelayedexpansion
+    @echo ALIASES=!ALIASES!
+    exit /b
+) 
+
+@REM if "%ALIASES%" == "" (
+if "%*" == "" if not defined ALIASES (
+    doskey ..=cd ..  
+    doskey host=cd C:\Users\ivanl\Documents\vl 
+    doskey pd=cd C:\Users\ivanl\Documents\vl\php\php-8.1 
+    doskey wd=cd %CD% 
+    @REM doskey com=@php "C:\Users\ivanl\Documents\vl\php\php-8.1\composer.phar" '%*'
+    doskey com=composer $*
+    doskey art=artisan $*
+    @set ALIASES=on
+    setlocal enabledelayedexpansion
+    @REM @echo ALIASES=!ALIASES!
+)
+@echo ALIASES=%ALIASES%
+exit /b
